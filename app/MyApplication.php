69c119064264ea1ec0ba46620cdc40390cd5a6fe
Honza <honzik.chal@gmail.com>
2025-11-22 12:38:43 +0100

    Kompletně hotovo i s dokumentací

--- app/MyApplication.php
+++ app/MyApplication.php
@@ -32,23 +32,6 @@ class MyApplication {
         $controller = new $controllerName();
         $controller->render();
     }
-
-//    public function renderTwig(string $template, array $data = [])
-//    {
-//        $data["app_base"] = BASE_URL;
-//        $data["session"] = $_SESSION ?? [];
-//
-//        $data["user"] = $_SESSION["user"] ?? null;
-//
-//        require_once __DIR__ . "/../vendor/autoload.php";
-//
-//        $loader = new \Twig\Loader\FilesystemLoader(TWIG_TEMPLATE_PATH);
-//        $twig = new \Twig\Environment($loader, [
-//            "debug" => true
-//        ]);
-//
-//        echo $twig->render($template, $data);
-//    }
     public function renderTwig(string $template, array $data = [])
     {
         require_once __DIR__ . "/../vendor/autoload.php";
@@ -58,12 +41,12 @@ class MyApplication {
             "debug" => true
         ]);
 
-        // ✅ bezpečné spuštění session
+        // bezpečné spuštění session
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
 
-        // ✅ předání uživatele a základní cesty do Twigu
+        // předání uživatele a základní cesty do Twigu
         $data["app_base"] = BASE_URL;
         $data["user"] = $_SESSION["user"] ?? null;
 
--- app/articlesView.php
+++
@@ -1,9 +0,0 @@
-<?php include "layout/header.php"; ?>
-
-<h1>Články</h1>
-
-<?php foreach ($articles as $a): ?>
-    <p><?= $a["title"] ?></p>
-<?php endforeach; ?>
-
-<?php include "layout/footer.php"; ?>
